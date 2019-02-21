3b7fa5e1cdbc964026cca8934e0a5a19c8578208
Xudko04031990 <kostya040390@gmail.com>
2019-02-21 10:12:28 +0200

    add close-item functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 'use strict'
 
-var panels = document.querySelectorAll('.panels__panel');
+var panels = document.querySelectorAll('.panels__panel'),
+panelsParent = document.querySelector('.panels');
 
-function toggleOpen() {
-    this.classList.toggle('open');
+
+function toggleOpen(e) {
+    var active = panelsParent.querySelector('.open');
+    if(active) {
+        active.classList.remove('open');
+        if(active !== this) {
+            this.classList.add('open');
+        }
+    }else {
+        this.classList.add('open');
+    }
 }
 
 function toggleActive(e) {
-    if(e.propertyName.includes('flex')) {
+    if(e.propertyName === 'flex' || e.propertyName === 'flex-grow') {
         this.classList.toggle('open-active');
     }
 }
